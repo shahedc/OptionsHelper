5e397dbbd77597b406dc8390a8491ac585f3eda5
shahedc <shahedc@gmail.com>
2023-03-10 22:59:10 -0500

    added OptionContract model

---
+++ OptionsHelperApp/Models/OptionContract.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OptionsHelperApp.Models
+{
+    public class OptionContract
+    {
+        [Key]
+        public int Id { get; set; }
+        public string ContractInfo { get; set; }
+        public string Symbol { get; set; }
+        public int Quantity { get; set; }
+        public string AccountOwner { get; set; }
+        public string OptionType { get; set; }
+        public double OpenAmount { get; set; }
+        public DateOnly DateOpened { get; set; }
+        public double ClosedAmount { get; set; }
+        public DateOnly ExpirationDate { get; set; }
+        public DateOnly ClosingDate { get; set; }
+        public double ProfitLoss { get; set; }
+        public double ProfitPercentage { get; set; }
+        public int DaysToExpiration { get; set; }
+
+        public bool Assigned { get; set; }
+        public double StrikePrice { get; set; }
+    }
+}
