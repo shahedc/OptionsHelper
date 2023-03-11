75d09748318a5c24a00afbebb5f18d1840b3ffe6
shahedc <shahedc@gmail.com>
2023-03-10 23:32:48 -0500

    migration for DTE rename

---
+++ OptionsHelperApp/Data/Migrations/20230311043159_RenamedDTE.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OptionsHelperApp.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenamedDTE : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DaysToExpiration",
+                table: "OptionContracts",
+                newName: "DaysToClose");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DaysToClose",
+                table: "OptionContracts",
+                newName: "DaysToExpiration");
+        }
+    }
+}
